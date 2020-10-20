c614fa4defe32950860f623406e0f39d44b5069e
Marko Tolevski <marko@tolevski.com>
2020-10-20 20:04:15 +0200

    Added .env support Set the env SIZE variable Added /park, /unpark, /info endpoints

--- server.js
+++ server.js
@@ -1,9 +1,28 @@
 const express = require('express');
 const app = express();
+const dotenv = require("dotenv");
+dotenv.config();
 const port = 3000;
 
+const PARKING = process.env.SIZE
+let parkingSpots = {};
+
+console.log(`Parking size set to ${PARKING}`)
+
 app.get('/', (req, res) => {
-    res.send('ON AIR!');
+    res.send('<h1>Parking API</h1>');
+});
+
+app.get('/park', (req, res) => {
+    
+});
+
+app.get('/unpark', (req, res) => {
+    
+});
+
+app.get('/info', (req, res) => {
+    
 });
 
 app.listen(port, () => console.log(`Server on ${port}!`))
